6e66b810deb8ef457f2e965d1efe1ef5ac7804f4
Jaiden Reddy <jaidenreddy@gmail.com>
2025-02-09 00:34:55 -0800

    no more eruda

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MiniKitProvider from "@/src/components/MiniKitProvider";
-import { ErudaDebug } from '@/src/components/ErudaDebug';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,7 +21,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <MiniKitProvider>{children}</MiniKitProvider>
-        <ErudaDebug />
       </body>
     </html>
   );
